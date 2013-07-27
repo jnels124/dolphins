5485339b6d3c48a377fbaa2e1ec73030486bff7c
Jesse Nelson <jnels124@msudenver.edu>
2013-07-27 00:28:27 -0600

    Updated system to awesome 3.5

--- theme.lua
+++ theme.lua
@@ -1,8 +1,9 @@
 -- arch, awesome3 theme
 
 --{{{ Main
-require("awful.util")
-
+--local awful = require("awful")
+--awful.util = 
+local awful = require("awful")
 theme = {}
 
 home          = os.getenv("HOME")
@@ -24,18 +25,15 @@ wallpaper1    = themedir .. "/background.jpg"
 wallpaper2    = themedir .. "/background.png"
 wallpaper3    = sharedthemes .. "/zenburn/zenburn-background.png"
 wallpaper4    = sharedthemes .. "/default/background.png"
-wpscript      = home .. "/.wallpaper"
 
 if awful.util.file_readable(wallpaper1) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper1 }
+    theme.wallpaper = wallpaper1
 elseif awful.util.file_readable(wallpaper2) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper2 }
-elseif awful.util.file_readable(wpscript) then
-	theme.wallpaper_cmd = { "sh " .. wpscript }
+    theme.wallpaper = wallpaper2
 elseif awful.util.file_readable(wallpaper3) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper3 }
+    theme.wallpaper = wallpaper3
 else
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper4 }
+    theme.wallpaper = wallpaper4
 end
 
 if awful.util.file_readable(config .. "/vain/init.lua") then
